33ea5d98eb1a50ec79d6f47c35455b4d64f9c473
ibbi-789 <cloudcomputingima@gmail.com>
2024-12-10 09:55:24 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Converter from './components/converter.jsx'
+import Converter from '/components/converter.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
